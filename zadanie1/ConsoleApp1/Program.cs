b8cab28f64e849847ab82df0205a41a7d43758ff
Andrei Lisenkov <s22235@pjwstk.edu.pl>
2022-03-10 11:38:52 +0100

    zadanie 1

---
+++ zadanie1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task main(string[] args)
+        {
+            var websiteURL = args[0];
+            HttpClient htttpClient = new HttpClient();
+            HttpResponseMessage response = await htttpClient.GetAsync(websiteURL);
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+
+            Regex regex = new Regex(@"");
+            MatchCollection matchCollection = regex.Matches(content);
+           
+            foreach (var item in matchCollection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
